7316901ba33d64c48614b4c4e832ae67887aecde
weston-ryan <31858652+weston-ryan@users.noreply.github.com>
2019-02-04 14:45:55 -0700

    Update Android.java

--- Android.java
+++ Android.java
@@ -3,23 +3,13 @@ package com.example.neldo.atm;
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
 import static org.junit.Assert.*;
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
 @RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
+public class AndroidExample {
     @Test
     public void useAppContext() throws Exception {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+               Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("com.example.neldo.atm", appContext.getPackageName());
     }
