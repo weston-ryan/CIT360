db5e88868893c2690abec45b96a8eddb9b1ad077
weston-ryan <31858652+weston-ryan@users.noreply.github.com>
2019-02-09 16:25:14 -0700

    Create Servlet.java

---
+++ Servlet.java
@@ -0,0 +1,32 @@
+
+/**
+ *
+ * @author westo
+ */
+public class Servlet {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //dispatches requests to the methods doGet and doPost
+        //httpServlet is a sublcass of GenericServlet with is the implementation of Servlet interface.
+        protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+    }
+        //hard coded username and password.  It will check if the user name and password match
+        // if they match it will proceed to the iccu-member page.  If not, it will redirect to the access denied page.
+    protected void dopost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String user = request.getParameter("user");
+                String password = request.getParameter("password");
+                if(user.equals("rweston") && password.equals("47LeguesUnder!")){
+                    response.setHeader("login", "success");
+                    response.sendRedirect("iccu-member.jsp")
+                    }           
+                else {
+                    response.sendRedirect("accessDenied.jsp");
+                }}
+    }
+                        
+}
