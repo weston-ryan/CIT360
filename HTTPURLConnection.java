35e11b434467422b4e99e5313922347ffe9da341
weston-ryan <31858652+weston-ryan@users.noreply.github.com>
2019-02-09 15:18:22 -0700

    Create HTTPURLConnection.java

---
+++ HTTPURLConnection.java
@@ -0,0 +1,30 @@
+/**
+ *
+ * @author westo
+ */
+package HTTPURLConnection;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class HTTPURLConnection {
+    public static void main(String[] args) {
+        try {
+            // the URL to the web sight where I work as an intern
+            URL url = new URL("https://www.iccu.com");
+            //open connection method used to communicate with web page
+            HttpURLConnection huc = (HttpURLConnection) url.openConnection();
+            /* This will pull up the first 20 header fields.  If there are
+             no more fields, then it will show "null = null"
+            */
+            for (int i = 1; i <= 20; i++) {
+                System.out.println(huc.getHeaderFieldKey(i) + " = " + huc.getHeaderField(i));
+            }
+            // Using a multi catch phrase exception and closing the connection.
+            huc.disconnect();
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+}
