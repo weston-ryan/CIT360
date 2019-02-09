960cfccd87ce51ebfd73afcb7fed1ad55ec99fdc
weston-ryan <31858652+weston-ryan@users.noreply.github.com>
2019-02-09 15:00:02 -0700

    Create OfficeArea.java

---
+++ OfficeArea.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ApplicationControlPatternSimplified;
+
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
+
+/**
+ *
+ * @author westo
+ */
+public class OfficeArea extends Handler {
+
+    public OfficeArea() {
+    }
+
+    @Override
+    public void publish(LogRecord record) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void flush() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
