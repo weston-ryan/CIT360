3ac6ae57ae9f30eacb6e036dd49fe7d3fb9f4961
weston-ryan <31858652+weston-ryan@users.noreply.github.com>
2019-03-20 12:23:13 -0600

    Create mappingFile.hbm.xml.java
    Mapping file

---
+++ WebApplication1/nbproject/mappingFile.hbm.xml.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author westo
+ */
+<?xml version = "1.0" encoding = "utf-8"?>
+<!DOCTYPE hibernate-mapping PUBLIC 
+"-//Hibernate/Hibernate Mapping DTD//EN"
+"http://www.hibernate.org/dtd/hibernate-mapping-3.0.dtd"> 
+
+<hibernate-mapping>
+   <class name = "Member" table = "MEMBER">
+      
+      <meta attribute = "class-description">
+         This class contains the member detail. 
+      </meta>
+      
+      <id name = "id" type = "int" column = "id">
+         <generator class="native"/>
+      </id>
+      
+      
+      <property name = "firstName" column = "first_name" type = "string"/>
+      <property name = "lastName" column = "last_name" type = "string"/>
+      <property name = "id" column = "id" type = "int"/>
+      
+   </class>
+</hibernate-mapping>
